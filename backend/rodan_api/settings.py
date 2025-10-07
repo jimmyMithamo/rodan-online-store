729f9d98e3b8c2788ac65c0a35bceaa80501f26e
jimmyMithamo <j1mithamo@gmail.com>
2025-10-07 11:34:23 +0300

    static files

--- backend/rodan_api/settings.py
+++ backend/rodan_api/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +128,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# The directory where WhiteNoise/Django will collect all static files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Optional: Add compression and caching support (Highly Recommended)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
